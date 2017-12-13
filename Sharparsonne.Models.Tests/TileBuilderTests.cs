9b2e06ce5984fa10bb7297378b64a6fcd6ab94eb
Lee Richardson <l.richardson@zoho.com>
2017-12-13 21:24:05 +0000

    Adds model test project and a test for TileBuilder.

---
+++ Sharparsonne.Models.Tests/TileBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Sharpasonne;
+using Sharpasonne.Models;
+using Xunit;
+
+namespace Sharparsonne.Models.Tests
+{
+    public class TileBuilderTests
+    {
+        [Fact]
+        void When_CreatingEmptyTile_Then_ReturnsSome()
+        {
+            var maybeTile = new TileBuilder()
+                .CreateTile(Enumerable.Empty<IFeature>());
+
+            Assert.True(maybeTile.HasValue);
+        }
+    }
+}
