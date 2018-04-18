8966075c2464382b79e95697a9ff3945ab0c8db2
Lee Richardson <LeeARichardson@users.noreply.github.com>
2018-04-18 19:51:59 +0100

    Fixes merge issue. (#18)

--- Sharpasonne/GameActions/PlaceTileGameAction.cs
+++ Sharpasonne/GameActions/PlaceTileGameAction.cs
@@ -19,20 +19,4 @@ namespace Sharpasonne.GameActions
             return new EngineState(board, engine);
         }
     }
-    
-    public class EngineState : IEngine
-    {
-        public EngineState(Board board, IEngine engine)
-        {
-            this.Board = board;
-            this.Rules = engine.Rules;
-            this.Players = engine.Players;
-            this.CurrentPlayerTurn = engine.CurrentPlayerTurn;
-        }
-
-        public Board Board { get; }
-        public Players Players { get; }
-        public int CurrentPlayerTurn { get; }
-        public IImmutableDictionary<Type, IImmutableList<IRule<IGameAction>>> Rules { get; }
-    }
 }
